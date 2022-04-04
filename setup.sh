69a03788b758f3e7961c49263d566158854be9c0
JoeyShapiro <joeyashapiro@gmail.com>
2022-04-04 14:07:14 -0500

    added setup, thanks to Ryan

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# you need conda and python (duh)
+
+# setup env
+conda create -n its472 python=3.9
+conda activate its472
+conda install pytorch torchvision torchaudio cudatoolkit11.3 -c pytorch
+conda install pandas matplotlib
+conda install -c dglteam dgl-cuda11.3
