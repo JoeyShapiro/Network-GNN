fa65a55eeb5466ad76a018ce24a669937b093513
JoeyShapiro <joeyashapiro@gmail.com>
2022-04-04 15:33:15 -0500

    added a heat map, i am so clever :P

--- network.py
+++ network.py
@@ -22,9 +22,29 @@ def build_graph():
     g.add_edges(src, dst)
     g.add_edges(dst, src)
 
-    return g
+    # get colors for a heatmap
+    max_edges = 0
+    best_node = edges[0]
+    colors = [] # good
+     
+    # get node with most connections
+    for i in edges:
+        curr_frequency = edges.count(i)
+        if(curr_frequency > max_edges):
+            max_edges = curr_frequency
+            best_node = i
+        
+    print("most common:", best_node, "; amount:", max_edges)
+    # create heat map
+    for e in edges:
+        amount = edges.count(e)
+        colors.append([amount/max_edges, 0, 0]) # create a shade of red, based on how often this one appears relative to most common
+        # ie if max_edges is 30 and this is 15, then this color will be [127.5, 0, 0], 0<c<1
+    print(len(edges), len(colors))
 
-G = build_graph()
+    return g, colors
+
+G, colors = build_graph() # outside function
 print('We have %d nodes.' % G.number_of_nodes())
 print('We have %d edges.' % G.number_of_edges())
 
@@ -34,6 +54,6 @@ import networkx as nx
 nx_G = G.to_networkx().to_undirected()
 # Kamada-Kawaii layout usually looks pretty for arbitrary graphs
 pos = nx.kamada_kawai_layout(nx_G)
-nx.draw(nx_G, pos, with_labels=True, node_color=[[.7, .7, .7]])
+nx.draw(nx_G, pos, with_labels=True, node_color=[[.7, .7, .7]], edge_color=colors)
 import matplotlib.pyplot as plt
 plt.show()
