4d4a56c575922ba4f78f4ffbcd1dbefa5e9ee0d6
JoeyShapiro <joeyashapiro@gmail.com>
2022-04-12 14:07:53 -0500

    added some type of prediction

--- network.py
+++ network.py
@@ -78,3 +78,90 @@ pos = nx.kamada_kawai_layout(nx_G)
 nx.draw(nx_G, pos, with_labels=True, node_color=[[.7, .7, .7]], edge_color=colors)
 import matplotlib.pyplot as plt
 plt.show()
+
+# Contruct a two-layer GNN model
+import dgl.nn as dglnn
+import torch.nn as nn
+import torch.nn.functional as F
+class SAGE(nn.Module):
+    def __init__(self, in_feats, hid_feats, out_feats):
+        super().__init__()
+        self.conv1 = dglnn.SAGEConv(
+            in_feats=in_feats, out_feats=hid_feats, aggregator_type='mean')
+        self.conv2 = dglnn.SAGEConv(
+            in_feats=hid_feats, out_feats=out_feats, aggregator_type='mean')
+
+    def forward(self, graph, inputs):
+        # inputs are features of nodes
+        h = self.conv1(graph, inputs)
+        h = F.relu(h)
+        h = self.conv2(graph, h)
+        return h
+
+import dgl.function as fn
+class DotProductPredictor(nn.Module):
+    def forward(self, graph, h):
+        # h contains the node representations computed from the GNN defined
+        # in the node classification section (Section 5.1).
+        with graph.local_scope():
+            graph.ndata['h'] = h
+            graph.apply_edges(fn.u_dot_v('h', 'h', 'score'))
+            return graph.edata['score']
+
+def construct_negative_graph(graph, k):
+    src, dst = graph.edges()
+
+    neg_src = src.repeat_interleave(k)
+    neg_dst = th.randint(0, graph.number_of_nodes(), (len(src) * k,))
+    return dgl.graph((neg_src, neg_dst), num_nodes=graph.number_of_nodes())
+
+class MLPPredictor(nn.Module):
+    def __init__(self, in_features, out_classes):
+        super().__init__()
+        self.W = nn.Linear(in_features * 2, out_classes)
+
+    def apply_edges(self, edges):
+        h_u = edges.src['h']
+        h_v = edges.dst['h']
+        score = self.W(th.cat([h_u, h_v], 1))
+        return {'score': score}
+
+    def forward(self, graph, h):
+        # h contains the node representations computed from the GNN defined
+        # in the node classification section (Section 5.1).
+        with graph.local_scope():
+            graph.ndata['h'] = h
+            graph.apply_edges(self.apply_edges)
+            return graph.edata['score']
+
+class Model(nn.Module):
+    def __init__(self, in_features, hidden_features, out_features):
+        super().__init__()
+        self.sage = SAGE(in_features, hidden_features, out_features)
+        self.pred = DotProductPredictor()
+    def forward(self, g, neg_g, x):
+        h = self.sage(g, x)
+        return self.pred(g, h), self.pred(neg_g, h)
+
+# ?
+G.ndata['feat'] = th.randn(142, 10)
+def compute_loss(pos_score, neg_score):
+    # Margin loss
+    n_edges = pos_score.shape[0]
+    return (1 - neg_score.view(n_edges, -1) + pos_score.unsqueeze(1)).clamp(min=0).mean()
+
+node_features = G.ndata['feat']
+n_features = node_features.shape[1]
+k = 5
+model = Model(n_features, 100, 100)
+opt = th.optim.Adam(model.parameters())
+for epoch in range(200):
+    negative_graph = construct_negative_graph(G, k)
+    pos_score, neg_score = model(G, negative_graph, node_features)
+    loss = compute_loss(pos_score, neg_score)
+    opt.zero_grad()
+    loss.backward()
+    opt.step()
+    print(epoch, "loss:", loss.item())
+
+node_embeddings = model.sage(G, node_features)
